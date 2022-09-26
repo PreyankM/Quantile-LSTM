1e3828cafadac61d10c1c2c01b1bf76f2562fa8f
PreyankM <78067405+PreyankM@users.noreply.github.com>
2022-09-26 23:39:33 +0530

    Update devnet_kdd19.py

--- Deviation Networks/devnet_kdd19.py
+++ Deviation Networks/devnet_kdd19.py
@@ -1,14 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-@author: Guansong Pang
-The algorithm was implemented using Python 3.6.6, Keras 2.2.2 and TensorFlow 1.10.1.
-More details can be found in our KDD19 paper.
-Guansong Pang, Chunhua Shen, and Anton van den Hengel. 2019. 
-Deep Anomaly Detection with Deviation Networks. 
-In The 25th ACM SIGKDDConference on Knowledge Discovery and Data Mining (KDD ’19),
-August4–8, 2019, Anchorage, AK, USA.ACM, New York, NY, USA, 10 pages. https://doi.org/10.1145/3292500.3330871
-"""
-
 import numpy as np
 
 np.random.seed(42)
